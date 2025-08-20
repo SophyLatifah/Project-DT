b069f86c9e427c0fcdfcaca09554aaa2217484aa
Sophy <sophy.latifah.22@gmail.com>
2025-08-20 18:13:08 +0700

    terakhir

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,9 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "src/pages/Home";
-import Login from "src/pages/Login";
-import Dashboard from "src/pages/Dashboard";
-import Order from "src/pages/Order";
-import Tracking from "src/pages/Tracking";
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import Dashboard from "../pages/Dashboard";
+import Order from "../pages/Order";
+import Tracking from "../pages/Tracking";
 
 export default function App() {
   return (
