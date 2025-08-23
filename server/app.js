ca1b263abd4cc7a38659806d777aeae31994c7c3
Gema Dodi Pranata <gemadp01@gmail.com>
2025-08-23 18:49:50 +0700

    server

---
+++ server/app.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const db = require("./db");
+var cors = require("cors");
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/profile", (request, response) => {
+  // ...
+});
+
+app.get("/order/:orderId", (request, response) => {
+  //
+});
+
+app.post("/order", (request, response) => {
+  //
+});
+
+app.get("/order", (request, response) => {
+  //
+});
+
+app.get("/layanan-laundry", (request, response) => {
+  //
+});
+
+app.put("/order/:id", (request, response) => {
+  //
+});
+
+app.get("/pembayaran", (request, response) => {
+  //
+});
+
+app.put("/pembayaran/:id", (request, response) => {
+  //
+});
+
+app.get("/layanan-laundry", (request, response) => {
+  //
+});
+
+app.post("/layanan-laundry", (request, response) => {
+  //
+});
+
+app.put("/layanan-laundry/:id", (request, response) => {
+  //
+});
+
+app.delete("/layanan-laundry", (request, response) => {
+  //
+});
+
+app.listen(PORT, () => {
+  console.log(`Server berjalan di http://localhost:${PORT}`);
+});
