d876a2b12de82beea222e1ddc7a994a4cae663a4
Sophy <sophy.latifah.22@gmail.com>
2025-08-24 19:45:33 +0700

    login

--- my-app/src/pages/ContactUs.jsx
+++ my-app/src/pages/ContactUs.jsx
@@ -21,5 +21,5 @@ export default function ContactUs() {
         </div>    
       </section>
     </div>
-  )
+  );
 }
--- my-app/src/pages/Login.jsx
+++ my-app/src/pages/Login.jsx
@@ -1,3 +1,35 @@
 export default function Login() {
-  return <h1>Login Page</h1>;
+  return (
+    <div>
+      <section className="mt-16 pt-10 flex-grow flex justify-center flex-col space-y-4 px-10">
+        <h1 className="text-3xl font-bold text-center text-[#023047] ">WELCOME TO KLEANZY!</h1>
+        <p className="mt-2 text-center text-gray-600">Solusi laundry praktis, cepat, dan terpercaya</p>
+      </section>
+
+      {/* Login Form */}
+      <div className="bg-[#8ECAE6] mx-auto mt-10 p-8 w-96 rounded-lg shadow-md">
+        <h2 className="text-center font-bold text-lg mb-6 text-white">LOGIN</h2>
+        <form>
+          <div className="mb-4">
+            <label className="block text-sm mb-1">Username :</label>
+            <div className="flex bg-white items-center rounded px-2 py-1">
+              <input type="text" className="flex-grow outline-none" placeholder="Enter Username" required />
+            </div>       
+          </div>
+          <div className="mb-6">
+            <label className="block text-sm mb-1">Password :</label>
+            <div className="flex bg-white items-center rounded px-2 py-1">
+              <input type="password" className="flex-row outline-none" placeholder="Enter Password" required />
+              <span className="material-icons text-gray-500"></span>
+            </div>
+          </div>
+          <div className="mb-4 flex items-center">
+            <input type="checkbox" id="remember" className="mr-2"/>
+            <label htmlFor="remember" className="text-sm">Remember Me?</label>
+          </div>
+          <button type="submit" className="bg-[#FFB701] w-full py-2 rounded text-white font-semibold">Login</button>
+        </form>
+      </div>
+    </div>
+  );
 }
