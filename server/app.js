6189b6e0a47bdfd8a1669eec5398044fa8b392b1
SophyLatifah <sophy.latifah.22@gmail.com>
2025-08-27 07:33:47 +0700

    database

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const db = require("./db");
 var cors = require("cors");
-
+const bodyParser = require("body-parser");
 const app = express();
 const PORT = 3000;
 
--- server/db.js
+++ server/db.js
@@ -1,1 +1,13 @@
 // Konfigurasi Database
+const mysql = require('mysql2');
+
+// Buat koneksi ke database MySQL
+const db = mysql.createPool({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'Sophybisa1!',
+    database: 'laundry_db'
+});
+
+module.exports = db.promise();
